ca10bdfc727ee91ee79c4266daf2bbf14a950356
SiqMello <91890617+Siqm@users.noreply.github.com>
2023-02-10 15:26:59 -0300

    Filmes, estilos e padrão da pagina

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -5,11 +5,13 @@ import styles from './styles.module.scss'
 
 export default function Home() {
 
-  const [movies, setMovies] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
 
   useEffect(() => {
 
-    async function loadMovies() {
+    async function loadNowPlaying() {
       const response = await api.get("movie/now_playing", {
         params: {
           api_key: process.env.REACT_APP_API_KEY,
@@ -17,30 +19,97 @@ export default function Home() {
         }
       })
 
-      const first10 = response.data.results.slice(0, 10);
-      const last10 = response.data.results.slice(10, 19);
+      console.log(response.data.results)
 
-      setMovies(first10)
+      setNowPlaying(response.data.results)
     }
 
-    loadMovies()
+    async function loadTopRated() {
+      const response = await api.get("/movie/top_rated", {
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+          page: 1
+        }
+      })
+
+      setTopRated(response.data.results)
+    }
+
+    async function loadUpcoming() {
+      const response = await api.get("/movie/upcoming", {
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+          page: 1
+        }
+      })
+
+
+
+      setUpcoming(response.data.results)
+    }
+
+    loadNowPlaying()
+    loadTopRated()
+    loadUpcoming()
+
+
   }, [])
+
 
   return (
     <div className={styles.container}>
-      <div className={styles.nowPlaying}>
-        {movies.map((movie) => {
+      <h1>Now Playing</h1>
+      <div className={styles.movieContainer}>
+
+        {topRated.map((movie) => {
+          return (
+/* cade o css  */
+
+            <article key={movie.id} className={styles.card}>
+              <h1>{movie.title}</h1>
+
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+                className={styles.poster}
+              />
+
+              <Link to={`/topRated/${movie.id}`}>Acessar</Link>
+
+            </article>
+
+
+          )
+        })}
+      </div>
+
+      {/* <div className={styles.movieContainer}>
+        {nowPlaying.map((movie) => {
           return (
             <article key={movie.id}>
               <strong>{movie.title}</strong>
 
               <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
 
-              <Link to={`/movies/${movie.id}`}>Acessar</Link>
+              <Link to={`/topRated/${movie.id}`}>Acessar</Link>
             </article>
           )
         })}
       </div>
+
+      <div className={styles.movieContainer}>
+        {upcoming.map((movie) => {
+          return (
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+
+              <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+
+              <Link to={`/topRated/${movie.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div> */}
     </div>
   )
 }
