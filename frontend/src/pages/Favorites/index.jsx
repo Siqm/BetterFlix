fbbba2572f7644e0b0c7b7e04091e0393543281c
SiqMello <91890617+Siqm@users.noreply.github.com>
2023-02-13 09:04:01 -0300

    Lógica visualizar/editar lista Salvos

--- frontend/src/pages/Favorites/index.jsx
+++ frontend/src/pages/Favorites/index.jsx
@@ -1,13 +1,54 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.scss'
 
 export default function Favorites() {
-	return (
-		<div className={styles.container}>
-			<div className={styles.main}>
-				<ul className={styles.list}>
-					
-				</ul>
-			</div>
-		</div>
-	)
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const myList = localStorage.getItem('@primeflix')
+    setMovies(JSON.parse(myList) || [])
+  },[])
+
+  function removeMovie(movieId) {
+    let movieFilter = movies.filter((item) => {
+      return(item.id !== movieId)
+    })
+
+    setMovies(movieFilter);
+    localStorage.setItem("@primeflix", JSON.stringify(movieFilter))
+    alert('Movie removed')
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <ul className={styles.list}>
+          {
+            movies.length === 0 &&
+            <span>Your saved movies list is empty :(</span>
+          }
+
+          <ul>
+            {
+              movies.map((i) => {
+                return (
+                  <li key={i.id}>
+                    <span>{i.title}</span>
+
+                    <div>
+                      <Link to={`/filmes/${i.id}`}>See details</Link>
+                      <button onClick={() => {removeMovie(i.id)}}>Remove</button>
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </ul>
+      </div>
+    </div>
+  )
 }
